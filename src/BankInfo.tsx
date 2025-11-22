77fb2ddff5962f735c678bf24cd115c086658874
ledsim <145004548+ledvisi22@users.noreply.github.com>
2025-11-22 21:44:49 +0100

    Pridana mena, kontrola nulovych uroku

--- src/BankInfo.tsx
+++ src/BankInfo.tsx
@@ -2,14 +2,28 @@ import type {BankDataType, BankSDType} from './Type';
 
 export function BankInfo({ bankData, depositMoney }: { bankData: BankSDType, depositMoney:number}) {
 
+  if (bankData.Interest == 0 || depositMoney == 0) {
+    return (
+    <>
+      <tr className='bank' id='tableNames'>
+        <td className='bankLogo'><img src={bankData?.Logo} alt={bankData.Name} title={bankData.Name} /></td>
+        <td className='bankInterest'>-</td>
+        <td className='bankInterestWithTax'>-</td>
+        <td className='bankTax'>-</td>
+        <td className='bankFinalMoney'>{Math.round(depositMoney + (bankData.Interest * depositMoney / 100 * 0.85))} CZK</td>
+      </tr>
+    </>
+    )
+  }
+
   return (
     <>
       <tr className='bank' id='tableNames'>
         <td className='bankLogo'><img src={bankData?.Logo} alt={bankData.Name} title={bankData.Name} /></td>
-        <td className='bankInterest'>{Math.round(bankData.Interest * depositMoney / 100)}</td>
-        <td className='bankInterestWithTax'>{Math.round(bankData.Interest * depositMoney * 0.85 / 100)}</td>
-        <td className='bankTax'>{Math.round(bankData.Interest * depositMoney / 100 * 0.15)}</td>
-        <td className='bankFinalMoney'>{Math.round(depositMoney + (bankData.Interest * depositMoney / 100 * 0.85))}</td>
+        <td className='bankInterest'>{Math.round(bankData.Interest * depositMoney / 100)} CZK</td>
+        <td className='bankInterestWithTax'>{Math.round(bankData.Interest * depositMoney * 0.85 / 100)} CZK</td>
+        <td className='bankTax'>{Math.round(bankData.Interest * depositMoney / 100 * 0.15)} CZK</td>
+        <td className='bankFinalMoney'>{Math.round(depositMoney + (bankData.Interest * depositMoney / 100 * 0.85))} CZK</td>
       </tr>
     </>
   );
