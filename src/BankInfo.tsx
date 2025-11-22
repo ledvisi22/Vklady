6b30999c190ff4b12af708c01ea6282511dfe7cf
ledsim <145004548+ledvisi22@users.noreply.github.com>
2025-11-22 22:26:59 +0100

    Upraven format men + aktualizace loga banek

--- src/BankInfo.tsx
+++ src/BankInfo.tsx
@@ -3,6 +3,9 @@ import type {BankSDType} from './Type';
 export function BankInfo({ bankData, depositMoney,best }: { bankData: BankSDType, depositMoney:number, best:boolean}) {
 
   if (bankData.Interest == 0 || depositMoney == 0) {
+    const bankFinalMoney = Intl.NumberFormat("cs-CZ",{ style: "currency", currency: "CZK" }).format(
+      0
+    )
     return (
     <>
       <tr className='bank'>
@@ -10,20 +13,33 @@ export function BankInfo({ bankData, depositMoney,best }: { bankData: BankSDType
         <td className='bankInterest'>-</td>
         <td className='bankInterestWithTax'>-</td>
         <td className='bankTax'>-</td>
-        <td className='bankFinalMoney'>{Math.round(depositMoney + (bankData.Interest * depositMoney / 100 * 0.85))} CZK</td>
+        <td className='bankFinalMoney'>{bankFinalMoney}</td>
       </tr>
     </>
     )
   }
 
+  const bankInterest = Intl.NumberFormat("cs-CZ",{ style: "currency", currency: "CZK" }).format(
+    bankData.Interest * depositMoney / 100
+  )
+  const bankInterestWithTax = Intl.NumberFormat("cs-CZ",{ style: "currency", currency: "CZK" }).format(
+    bankData.Interest * depositMoney * 0.85 / 100
+  )
+  const bankTax = Intl.NumberFormat("cs-CZ",{ style: "currency", currency: "CZK" }).format(
+    bankData.Interest * depositMoney / 100 * 0.15
+  )
+  const bankFinalMoney = Intl.NumberFormat("cs-CZ",{ style: "currency", currency: "CZK" }).format(
+    depositMoney + (bankData.Interest * depositMoney / 100 * 0.85)
+  )
+
   return (
     <>
       <tr className={best ? "bestBank bank" : "bank"}>
         <td className='bankLogo'><img src={bankData?.Logo} alt={bankData.Name} title={bankData.Name} /></td>
-        <td className='bankInterest'>{Math.round(bankData.Interest * depositMoney / 100)} CZK</td>
-        <td className='bankInterestWithTax'>{Math.round(bankData.Interest * depositMoney * 0.85 / 100)} CZK</td>
-        <td className='bankTax'>{Math.round(bankData.Interest * depositMoney / 100 * 0.15)} CZK</td>
-        <td className='bankFinalMoney'>{Math.round(depositMoney + (bankData.Interest * depositMoney / 100 * 0.85))} CZK</td>
+        <td className='bankInterest'>{bankInterest}</td>
+        <td className='bankInterestWithTax'>{bankInterestWithTax}</td>
+        <td className='bankTax'>{bankTax}</td>
+        <td className='bankFinalMoney'>{bankFinalMoney}</td>
       </tr>
     </>
   );
