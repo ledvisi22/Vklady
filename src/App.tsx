d3df743f3bbb16ffdd45c17ff1881f3b7a87f6dd
ledsim <145004548+ledvisi22@users.noreply.github.com>
2025-11-23 22:24:33 +0100

    Vylepsen input na vklady

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import './css/buttons.css';
+import './css/table.css';
 import bankData from './assets/banks.json';
 import type {BankDataType}  from './Type';
 import { BankTable } from './BankTable';
@@ -17,7 +18,8 @@ function App() {
       <div id='depositDiv'>
         <div>
           <label htmlFor="moneyDeposit">Vklad: </label>
-          <input onChange={e=>setDepositMoney(isNaN(parseInt(e.target.value)) ? 0 : parseInt(e.target.value) >= 0 ? parseInt(e.target.value) : 0)} type="number" name="moneyDeposit" id="moneyDeposit" />
+          <input onChange={e=>handleDepositInput(e.target.value, e.target)} type="text" name="moneyDeposit" id="moneyDeposit" />
+          <label htmlFor="moneyDeposit"> Kč</label>
         </div>
         <div id='timeSelector'>
           <button onClick={()=>{setTime(3)}} className={ time === 3 ? 'selected' : '' }>3 měsíce</button>
@@ -43,6 +45,26 @@ function App() {
 
     </>
   )
+
+  function handleDepositInput(depositMoney:string, inputElem:HTMLInputElement ) {
+
+    const inputNum = parseInt(depositMoney.replaceAll(" ",""));
+
+    if (isNaN(inputNum)) {
+      setDepositMoney(0);
+      return;
+    }
+    if (inputNum >= 0) {
+      inputElem.value = new Intl.NumberFormat("cs-CZ").format(inputNum)
+      setDepositMoney(inputNum);
+      return;
+    }
+    setDepositMoney(0);
+    return;
+
+  }
+
+
 }
 
 export default App
